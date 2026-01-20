858c6a03caf899d0ea63f5cb74314c06dcc3490a
Adam Burdett <burdettadam@gmail.com>
2026-01-20 15:58:36 -0700

    style(oid4vc): fix line length violations in public_routes/__init__.py
    Signed-off-by: Adam Burdett <burdettadam@gmail.com>

--- oid4vc/oid4vc/public_routes/__init__.py
+++ oid4vc/oid4vc/public_routes/__init__.py
@@ -1,7 +1,8 @@
 """Public routes package.
 
-This package is being incrementally built to replace the monolithic public_routes.py module.
-For now, to avoid circular imports, the register function is imported using sys.modules manipulation.
+This package is being incrementally built to replace the monolithic
+public_routes.py module. For now, to avoid circular imports, the register
+function is imported using sys.modules manipulation.
 """
 
 import sys
@@ -14,14 +15,17 @@ def __getattr__(name):
     """Lazy import to avoid circular dependency."""
     if name == "register":
         # At this point, oid4vc.public_routes refers to THIS package.
-        # We need to get the .py module which Python loaded as part of oid4vc.
-        # When Python imported oid4vc, it loaded public_routes.py first before this package.
-        # But then it replaced sys.modules['oid4vc.public_routes'] with this package.
+        # We need to get the .py module which Python loaded as part of
+        # oid4vc. When Python imported oid4vc, it loaded public_routes.py
+        # first before this package. But then it replaced
+        # sys.modules['oid4vc.public_routes'] with this package.
         # So we reload the .py file explicitly.
         import importlib.util
         from pathlib import Path
         _module_path = Path(__file__).parent.parent / "public_routes.py"
-        _spec = importlib.util.spec_from_file_location("oid4vc._public_routes_py", _module_path)
+        _spec = importlib.util.spec_from_file_location(
+            "oid4vc._public_routes_py", _module_path
+        )
         _module = importlib.util.module_from_spec(_spec)
         # Set as oid4vc parent so relative imports work
         _module.__package__ = "oid4vc"
