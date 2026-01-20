dcbaea350bc013f9f2bdfc7034df5718d2857a1a
Adam Burdett <burdettadam@gmail.com>
2026-01-20 14:20:24 -0700

    fix(oid4vc): re-export register function in public_routes package
    The public_routes package shadows the public_routes.py module.
    Re-export the register function so imports continue to work.
    
    Signed-off-by: Adam Burdett <burdettadam@gmail.com>

--- oid4vc/oid4vc/public_routes/__init__.py
+++ oid4vc/oid4vc/public_routes/__init__.py
@@ -1,1 +1,8 @@
 """Public routes package."""
+
+# Re-export register function from the parallel public_routes.py module
+# This allows imports like `from .public_routes import register` to work
+# even though public_routes is now also a package (directory)
+from ..public_routes import register
+
+__all__ = ["register"]
